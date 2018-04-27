9e99d74e6dd6447b3c3927666d95dd0e5c718973
My Kim Bui <mykimbuibui@gmail.com>
2018-04-27 17:40:10 +0200

    hide

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,12 +1,35 @@
 var photo = document.querySelectorAll('.photo');
 var city = document.querySelectorAll('.city-name');
 
-city.forEach(function(element){
-  element.addEventListener('click',function(){
-    document.getElementById("marque").classList.toggle("hidden");
-    photo.forEach(function(element){
+// city.forEach(function(element){
+//   element.addEventListener('click',function(){
+//     document.getElementById("marque").classList.toggle("hidden");
+//     photo.forEach(function(element){
+//       element.classList.toggle("hidden");
+//     });
+//     console.log("yo");
+//   });
+// });
+
+
+// for (var i = 0, total = city.length; i < total; i++) {
+//   (function(index){
+//     city[i].onclick = function(){
+//       alert(index)  ;
+//     }
+//   })(i);
+// }
+
+
+for (var i = 0, total = city.length; i < total; i++) {
+  (function(index){
+    city[i].onclick = function(){
+      document.getElementById("marque").classList.toggle("hidden");
+      photo.forEach(function(element){
         element.classList.toggle("hidden");
-    });
-    console.log("yo");
-  });
-});
+      });
+      console.log(index);
+    }
+  })(i);
+}
+
