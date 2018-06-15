423bb84153a4a68b64a56c3da69b1886ed6adf8b
My Kim Bui <mykimbuibui@gmail.com>
2018-06-15 14:37:04 +0200

    hover me

---
+++ app/assets/javascripts/hover.js
@@ -0,0 +1,20 @@
+var city = document.querySelectorAll('.city-name');
+var pic = document.querySelectorAll('.hovering');
+
+city.forEach(function(element){
+  element.addEventListener('mouseover',function(element){
+    console.log("im hoverin");
+    element.target.style.color = "red";
+    pic.forEach(function(element){
+      element.classList.remove("hidden");
+    });
+    setTimeout(function() {
+      element.target.style.color = "";
+    }, 500);
+  }, false);
+  element.addEventListener('mouseleave',function(element){
+    pic.forEach(function(element){
+      element.classList.add("hidden");
+    });
+  }, false);
+});
