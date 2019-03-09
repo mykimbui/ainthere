066a986bc2c0f894415bb90d431add7969d8a237
My Kim Bui <mykimbuibui@gmail.com>
2019-03-09 16:27:20 +0100

    precompile

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Ainthere
   class Application < Rails::Application
     config.generators do |generate|
@@ -16,6 +17,7 @@ module Ainthere
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
